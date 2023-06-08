1f2ba84b7b8e8511da846569ae7812c06ba2c988
Akash <abd.akashraj519@gmail.com>
2023-06-08 07:29:44 +0530

    encapsulation problem is solved in spring basic

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String a[])
+  {
+      RWOnly r1 = new RWOnly();
+     // r1.name="Akash"; //name has private access in com.driver.RWOnly
+      r1.setName("Akash");
+      System.out.println(r1.getName());
+  }
 }
